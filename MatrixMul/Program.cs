fdcfed7d2032d505fe8fc16fb196e23bcab77351
qwerty34-qw <wh861440@gmail.com>
2020-11-26 22:45:30 +0200

    Add project files.

---
+++ MatrixMul/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MatrixMul
+{
+    class Program
+    {
+        Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            
+            while(true)
+            {
+                Console.WriteLine("Enter rows of 1 matrix: ");
+                int row1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter columns of 1 matrix: ");
+                int col1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter rows of 2 matrix: ");
+                int row2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter columns of 2 matrix: ");
+                int col2 = Convert.ToInt32(Console.ReadLine());
+
+
+                int[,] matrix1 = new int[row1, col1];
+                int[,] matrix2 = new int[row2, col2];
+
+
+
+
+            }
+
+        }
+
+        private void FillMatrix()
+        {
+
+        }
+
+    }
+}
