b6679d57653c816615a10f659847417450c103cd
qwerty34-qw <wh861440@gmail.com>
2020-11-27 11:46:39 +0200

    Create Matrix class. Multiply matrix

---
+++ MatrixMul/Matrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MatrixMul
+{
+    public class Matrix: IEnumerable
+    {
+        public int[,] matrix;
+        public int rows { get; set; }
+        public int cols { get; set; }
+
+        public Matrix(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+            this.rows = rows;
+            this.cols = cols;
+        }
+
+        public void FillMatrix()
+        {
+            Random rnd = new Random();
+
+            Parallel.For(0, rows, i =>
+                {
+                    for (int j = 0; j < cols; j++)
+                        matrix[i, j] = rnd.Next(1, 100);
+                }
+            );
+        }
+
+        public void ShowMatrix()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j].ToString() + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public void Show(Matrix matrix)
+        {
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return matrix.GetEnumerator();
+        }
+
+    }
+}
--- MatrixMul/Program.cs
+++ MatrixMul/Program.cs
@@ -1,43 +1,113 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace MatrixMul
 {
     class Program
     {
-        Random rnd = new Random();
+        static Random rnd = new Random();
 
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter 0 for exit");
+            Console.WriteLine("Enter -1 to clear console\n\n");
             while(true)
             {
-                Console.WriteLine("Enter rows of 1 matrix: ");
+                Console.Write("Enter rows of 1 matrix: ");
                 int row1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter columns of 1 matrix: ");
+                if (row1 == 0)
+                    break;
+                else if (row1 == -1)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.Write("Enter columns of 1 matrix: ");
                 int col1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter rows of 2 matrix: ");
+                if (col1 == 0)
+                    break;
+                else if (col1 == -1)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.Write("Enter rows of 2 matrix: ");
                 int row2 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter columns of 2 matrix: ");
+                if (row2 == 0)
+                    break;
+                else if (row2 == -1)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.Write("Enter columns of 2 matrix: ");
                 int col2 = Convert.ToInt32(Console.ReadLine());
 
+                if (col2 == 0)
+                    break;
+                else if (col2 == -1)
+                {
+                    Console.Clear();
+                    continue;
+                }
 
-                int[,] matrix1 = new int[row1, col1];
-                int[,] matrix2 = new int[row2, col2];
+
+                Matrix matrix1 = new Matrix(row1, col1);
+                Matrix matrix2 = new Matrix(row2, col2);
+
+                matrix1.FillMatrix();
+                matrix2.FillMatrix();
+
+                Console.WriteLine();
+
+                matrix1.ShowMatrix();
+
+                Console.WriteLine("\n\n\n");
+
+                matrix2.ShowMatrix();
+
+                Matrix result_matrix = MultiplyMatrix(matrix1, matrix2);
 
 
+                Console.WriteLine("\n\n\tResult\n\n");
+                Console.WriteLine("############################################################\n");
 
+                result_matrix.ShowMatrix();
 
+                Console.WriteLine("############################################################");
             }
 
+            Console.WriteLine("\n\n\tEXIT\n\n");
+
         }
 
-        private void FillMatrix()
+        static public Matrix MultiplyMatrix(Matrix matrix1, Matrix matrix2)
         {
+            if (matrix1.cols != matrix2.rows)
+                throw new Exception("Matrix1 columns not equal Matrix2 rows");
+
+            Matrix matrix_res = new Matrix(matrix1.rows, matrix2.cols);
+
+            Parallel.For(0, matrix1.rows, i =>
+                {
+                    for (int j = 0; j < matrix2.cols; j++)
+                        for (int k = 0; k < matrix1.cols; k++)
+                            matrix_res.matrix[i, j] += matrix1.matrix[i, k] * matrix2.matrix[k, j];
+                }
+            );
+
+            return matrix_res;
 
         }
+     
 
     }
 }
